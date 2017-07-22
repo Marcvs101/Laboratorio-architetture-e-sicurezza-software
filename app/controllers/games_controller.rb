d6a373c04b127487b1aaecd99abf1636f92f01af
Luca Carbone <lucacarbone85@yahoo.it>
2017-07-22 23:25:13 +0200

    game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,79 @@
 class GamesController < ApplicationController
+    def index
+        @games = Game.all
+    end
+    
+    def show
+        id = params[:id]
+        @game = Game.find(id)
+    end
+    
+    def new
+        @game = Game.new
+    end
+    
+    def create
+        params.require(:game)
+        params.permit!
+        
+        #controllo: il nome non deve essere già presente
+        valid = true
+        array = Game.all
+        array.each do |game|
+            if game.name == params[:game][:name]
+                flash[:warning] = "#{params[:game][:name]} already exist"
+                valid = false
+            end
+        end
+        
+        #controllo: i campi non devono essere vuoti
+        if params[:game][:name].length == 0
+            flash[:warning] = "Name cant be blank"
+            valid = false
+        
+        elsif params[:game][:genre].length == 0
+            flash[:warning] = "Genre cant be blank"
+            valid = false
+        
+        elsif params[:game][:maker].length == 0
+            flash[:warning] = "Maker cant be blank"
+            valid = false
+        
+        #elsif params[:game][:year].length == 0
+        #    flash[:warning] = "Year cant be blank"
+        #    valid = false
+        
+        elsif params[:game][:description].length == 0
+            flash[:warning] = "Description cant be blank"
+            valid = false
+        end
+        
+        #
+        if valid
+            @game = Game.create!(params[:game])
+            flash[:notice] = "#{@game.name} was added"
+        end
+        redirect_to games_path
+    end
+    
+    def edit
+        @game = Game.find params[:id]
+    end
+    
+    def update
+        params.require(:game)
+        params.permit!
+        @game = Game.find params[:id]
+        @game.update_attributes!(params[:game])
+        flash[:notice] = "#{@game.name} was successfully updated"
+        redirect_to game_path(@game)
+    end
+    
+    def destroy
+        @game = Game.find(params[:id])
+        @game.destroy
+        flash[:notice] = "Game '#{@game.name}' removed"
+        redirect_to games_path
+    end
+        
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,15 +1,21 @@
-class Game < Reportable
-    attr_accessor :name, :description, :genre, :year, :maker, :pegi
+#class Game < Reportable
+#    attr_accessor :name, :description, :genre, :year, :maker, :pegi
+#    belongs_to :user
+#    has_many :reviews
+#    has_many :ads
+
+#    def self.search(target)
+#        where("name LIKE ?", "%#{target}%")
+#        where("description LIKE ?", "%#{target}%")
+#        where("genre LIKE ?", "%#{target}%")
+#        where("year LIKE ?", "%#{target}%")
+#        where("maker LIKE ?", "%#{target}%")
+#        where("pegi LIKE ?", "%#{target}%")
+#    end
+#end
+
+class Game < ActiveRecord::Base
     belongs_to :user
     has_many :reviews
     has_many :ads
-
-    def self.search(target)
-        where("name LIKE ?", "%#{target}%")
-        where("description LIKE ?", "%#{target}%")
-        where("genre LIKE ?", "%#{target}%")
-        where("year LIKE ?", "%#{target}%")
-        where("maker LIKE ?", "%#{target}%")
-        where("pegi LIKE ?", "%#{target}%")
-    end
 end
