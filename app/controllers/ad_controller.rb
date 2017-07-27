3ceaeb6ff23cdf110627311efe3fb0414ceecb04
Francesco994 <francesco.pernice11@gmail.com>
2017-07-27 16:28:20 +0200

    Aggiunto Delete al controller Ad e finite le view

--- app/controllers/ad_controller.rb
+++ app/controllers/ad_controller.rb
@@ -9,20 +9,44 @@ class AdController < ApplicationController
         	@ad = Ad.find(id)
 	end
 
-	def new
-        maker = current_user
-        	if maker == nil
-        		flash[:warning] = 'You must be logged in to add ads'
-        		redirect_to ads_path
-        	end
-        @ad = Ad.new
+	def new   
     	end
 
-	def show
-		@ad = Ad.find(params[:id])
-
 	def create
+		@game=Game.find[:game_id]
+		maker = current_user
+        		if maker == nil
+        			flash[:warning] = 'You must be logged in to add ads'
+        			redirect_to ads_path
+        		end
+		
+		@ad=Ad.new(report_adparams)
+		maker.ads << @ad
+		@game.ads << @ad
 	end
+
+	def destroy
+        @ad = Ad.find(params[:id])
+        user_target = current_user
+        if (user_target == nil)
+            flash[:warning] = 'You must be logged in to remove ads'
+            redirect_to ads_path
+        elsif (user_target.games.any? {|game| game.name == @game.name} || user_target.role == 'Admin')
+            @ad.destroy
+            flash[:notice] = "Ad '#{@ad.name}' removed"
+            redirect_to ads_path
+        else
+            flash[:warning] = 'cant remove a game if you have not created it'
+            redirect_to ads_path
+        end
+    end
+
+	
+
+	private
+        def report_adparams
+        	params.require(:ad).permit(:description, :type, :location, :delivery) 
+        end
 
 
 end
