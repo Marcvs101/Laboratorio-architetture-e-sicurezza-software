b8c675071735c28ad3cb8cbc3dc6e2b4ec007e8f
Marcvs101 <marcocuoci@yahoo.it>
2017-07-29 15:57:08 +0200

    Risolto merge

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -12,6 +12,18 @@ class ReportsController < ApplicationController
         if (current_user && current_user.role == "Admin")
             @report = Report.find(params[:id])
             @from = User.find(@report.user_id)
+            @target = nil
+            @type = nil
+            if @report.game_id != nil
+                @target = Game.find(@report.game_id)
+                @type = "Game"
+            elsif @report.ad_id != nil
+                @target = Ad.find(@report.ad_id)
+                @type = "Ad"
+            elsif @report.review_id != nil
+                @target = Review.find(@report.review_id)
+                @type = "Review"
+            end
         else
             flash[:warning] = "How did you even get here?"
             redirect_to games_path
@@ -24,19 +36,26 @@ class ReportsController < ApplicationController
     def create
         user_id = current_user
         target = nil
-        if params[:game_id]
-            target = Game.find(params[:game_id])
-        elsif params[:ad_id]
-            target = Ad.find(params[:ad_id])
-        elsif params[:review_id]
-            target = Review.find(params[:review_id])
+        debug = "out of if"
+        if params[:game_target] != nil
+            debug = "if game"
+            target = Game.find(params[:game_target])
+        elsif params[:ad_target] != nil
+            debug = "if ad"
+            target = Ad.find(params[:ad_target])
+        elsif params[:review_target] != nil
+            debug = "if review"
+            target = Review.find(params[:review_target])
         end
+        debug = debug + " '" + params[:game_target].to_s + "',"
+        debug = debug + "'" + params[:ad_target].to_s + "',"
+        debug = debug + "'" + params[:review_target].to_s + "' "
 
         if (current_user == nil || current_user.role != "Admin")
             flash[:warning] = "How did you even get here?"
             redirect_to games_path
         else
-            if user_id && target
+            if user_id && (target != nil)
                 @report = Report.new(report_params)
                 user_id.reports << @report
                 target.reports << @report
@@ -44,7 +63,11 @@ class ReportsController < ApplicationController
                 flash[:warning] = "Report sent succesfully"
                 redirect_to games_path
             else
-                flash[:warning] = "Something went wrong"
+                if user_id
+                    flash[:warning] = "FUKKIN TARGET: "+debug+"//"+params.inspect
+                else
+                    flash[:warning] = "REGISTER YOU FOOL"
+                end
                 redirect_to games_path
             end
         end
