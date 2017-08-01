eafae584ba2a9f32a428ff402a81c7122aa7cd2f
Marcvs101 <marcocuoci@yahoo.it>
2017-08-01 17:03:34 +0200

    Fix sulle routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
 
-  devise_for :users
-    devise_for :users
     #roba per facebook
     devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
-    resources :users
     resources :games do
         resources :reviews
         resources :ads
