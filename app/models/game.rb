4046209cf6696bf6f6fe2dc87a794a15c5030585
Luca Carbone <lucacarbone85@yahoo.it>
2017-08-23 18:10:53 +0200

    bug fix

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,8 +1,7 @@
 class Game < ActiveRecord::Base
     def self.all_pegi ; %w[3 7 12 16 18] ; end
 
-    validates :name, presence: true
-    validate :not_already_existing
+    validates :name, presence: true, uniqueness: true
     validates :description, presence: true
     validates :maker, presence: true
     validates :genre, presence: true
@@ -12,11 +11,6 @@ class Game < ActiveRecord::Base
     has_many :reviews, :dependent => :destroy
     has_many :ads, :dependent => :destroy
     has_many :reports, :dependent => :destroy
-    
-    def not_already_existing
-        errors.add(:name, 'game already exist') if
-            Game.where(:name => name).length > 0
-    end
 
     def self.search(target)
         where("name LIKE ? OR description LIKE ? OR genre LIKE ? OR year LIKE ? OR maker LIKE ? OR pegi LIKE ?", target[:parameter], target[:parameter], target[:parameter], target[:parameter], target[:parameter], target[:parameter])
